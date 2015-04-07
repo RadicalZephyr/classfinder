33e180f463cd8dca64038d2a84cc2ff02a0d8a4d
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-07 03:43:39 -0500

    Add more potential dependencies

--- project.clj
+++ project.clj
@@ -6,8 +6,13 @@
                  [ring/ring-core "1.3.2"]
                  [ring/ring-defaults "0.1.2"]
                  [compojure "1.3.1"]
-                 [liberator "0.12.2"]]
+                 [liberator "0.12.2"]
+                 [net.kronkltd/octohipster "0.3.0-SNAPSHOT"]
+                 [clj-http "1.1.0"]
+                 [enlive "1.1.5"]]
+
   :plugins [[lein-ring "0.8.13"]]
+
   :ring {:handler classfinder.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
