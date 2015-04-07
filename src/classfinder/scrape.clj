e3d18a3a41dd8b084f4a414ea7840b78dbe3c1d5
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-07 16:23:12 -0500

    Add functions for retrieving different options
    Scrape the form submit page for the lists of options

--- src/classfinder/scrape.clj
+++ src/classfinder/scrape.clj
@@ -2,6 +2,8 @@
   (:require [clj-http.client :as client]
             [net.cgrand.enlive-html :as html]))
 
+(def base-url "https://admin.wwu.edu/pls/wwis/wwsktime.SelClass")
+
 (defn fetch-url [url req-opts]
   (when (:method req-opts)
     (some-> req-opts
@@ -9,3 +11,19 @@
             client/request
             :body
             html/html-snippet)))
+
+(defn get-list [select-name]
+  (html/select (fetch-url base-url {:method :get})
+               [[:select (html/attr= :name select-name)] :option]))
+
+(defn get-terms []
+  (get-list "term"))
+
+(defn get-subjects []
+  (get-list "sel_subj"))
+
+(defn get-course-attributes []
+  (get-list "sel_gur"))
+
+(defn get-instructors []
+  (get-list "sel_inst"))
